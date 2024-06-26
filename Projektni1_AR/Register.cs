7115b5880855a4966370b16570be3cb8c8b1b195
Dejana Smiljanic <dejana.smiljanic@student.etf.unibl.org>
2024-06-26 14:38:43 +0200

    Add project files.

---
+++ Projektni1_AR/Register.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektni1_AR
+{
+    public abstract class Register
+    {
+        protected ulong value;
+        public ulong Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+        public void Reset ()
+        {
+            value = 0;
+        }
+
+    }
+}
